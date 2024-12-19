87d7bf80a572e9cb397b4b62b626a9755bb7806c
Duc Anh <anhdd.23bi14012@usth.edu.vn>
2024-12-19 15:42:33 +0700

    first commit

---
+++ Hello.py
@@ -0,0 +1,25 @@
+i=41
+print( type(i))
+print('Hi'*5)
+s = "Advanced Programming with Python"
+print(s[9])
+name='Duc Anh'
+age=10
+print(f"Greeting,{name}.You are {age}")# this is a comment line
+
+
+x = input (int)
+y=4
+if (y < 10 ):
+    print('Smaller than 10')
+else:
+     print('Bigger than 10')
+print('End')
+def greet(name):
+    """
+This function greets to
+the person passed in as
+a parameter
+"""
+print("Hello, " + name + ". Good morning!")
+greet("Emmanuel Macron")
